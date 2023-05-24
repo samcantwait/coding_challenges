b0024ed3b5a24579679efa4f8a85c379a439ae95
Samantha Atkins <samatkins@gmail.com>
2023-05-24 09:53:08 -0500

    remove commented code from diagonal difference solution

--- week_02/diagonalDifference.js
+++ week_02/diagonalDifference.js
@@ -8,12 +8,6 @@ function diagonalDifference(arr) {
         sumSecondary += arr[i][j];
     }
     return Math.abs(sumPrimary - sumSecondary);
-
-    // for (let i = 0, j = n - 1; i < arr.length; i += n + 1, j += n - 1) {
-    //     sumPrimary += arr[i];
-    //     sumSecondary += arr[j]
-    // }
-    // return Math.abs(sumPrimary - sumSecondary);
 }
 
 console.log(diagonalDifference([[11, 2, 4], [4, 5, 6], [10, 8, -12]]))
